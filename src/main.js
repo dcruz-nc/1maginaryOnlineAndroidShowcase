5e3e56cfa6d94caa1f138a5ddafa35b2bc6a58fc
David Cruz <dcruz.nc@gmail.com>
2025-08-30 06:20:48 -0400

    header z fix

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ document.querySelector('#root').innerHTML = `
     </div>
 
     <!-- Navigation -->
-    <nav class="bg-black border-b border-green-400 sticky top-0 z-40">
+    <nav class="bg-black border-b border-green-400 sticky top-0">
       <div class="max-w-7xl mx-auto px-4">
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center">
